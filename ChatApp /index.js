e5f586a812110bbc4b3519f06009a571b226d28f
aron mebrahtu <mebrah2@gmail.com>
2018-11-05 05:17:04 -0600

    CA-102 fix syntax

--- ChatApp /index.js
+++ ChatApp /index.js
@@ -15,7 +15,7 @@ app.use(express.static('website'));
 
 
 //Setting up socket
-var io = socket('server');
+var io = socket(server);
 io.on('connection', function(socket){
 
 console.log("ChatAppServer: backend server connection on");
